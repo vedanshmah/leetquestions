dddf7b2487ac832448e2a5e75da84cfd0e813da7
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2024-10-07 06:11:07 +0530

    Time: 14 ms (8.35%), Space: 12.2 MB (7.22%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minLength(string s) {
         stack<int> ans;
-        
         for(int i=0;i<s.length();i++){
             if(s[i]=='B' && ans.size()>0 && ans.top()=='A'){
                 ans.pop();
