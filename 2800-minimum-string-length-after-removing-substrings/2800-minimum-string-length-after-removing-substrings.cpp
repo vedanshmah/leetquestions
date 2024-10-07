6f6bac1a12733bad7d25e5c6d26829dfb813d346
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2024-10-07 06:10:28 +0530

    Time: 8 ms (42.58%), Space: 12 MB (11.13%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<int> ans;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='B' && ans.size()>0 && ans.top()=='A'){
+                ans.pop();
+            }
+            else if(s[i]=='D' && ans.size()>0 && ans.top()=='C'){
+                ans.pop();
+            }
+            else{
+                ans.push(s[i]);
+            }
+        }
+        return ans.size();
+    }
+};
