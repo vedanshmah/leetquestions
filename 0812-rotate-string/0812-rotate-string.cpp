cbfe64b9b431afc2294d5e9b47268b149ae9e8b5
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2024-11-04 02:29:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (55.5%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -5,15 +5,16 @@ public:
         if(s.length()!=goal.length()){
             return false;
         }
-        string storage=s+s;
-        //int j=0;
-        //for(int i=0;i<n;i++){
-        //    storage[n+i]=s[i];
+        //string storage=s+s;
+        //if(storage.find(goal)!=string::npos){
+        //    return true;
         //}
-        if(storage.find(goal)!=string::npos){
-            return true;
+        for(int i=0;i<n;i++){
+            rotate(s.begin(),s.begin()+1,s.end());
+            if(s==goal){
+                return true;
+            }
         }
-        
         return false;
         
     }
