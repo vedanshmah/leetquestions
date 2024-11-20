c85dc429f2567746a458652b3b6d251fb47cbca3
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2024-11-21 01:10:31 +0530

    Time: 11 ms (71.48%), Space: 12 MB (80.2%) - LeetHub

--- 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
@@ -3,6 +3,7 @@ public:
     int takeCharacters(string s, int k) {
         int n=s.length();
         int co_a=0,co_b=0,co_c=0;
+        //pehle a b c ka count nikalo
         for(int i=0;i<n;i++){
             if(s[i]=='a'){
                 co_a++;
@@ -14,13 +15,15 @@ public:
                 co_c++;
             }
         }
+        //agar count k se kam hai then return -1 kro
         if(co_a < k || co_b < k ||co_c < k){
             return -1;
         }
-
+        
         int i=0,j=0;
         int windowsize=INT_MIN;
         while(j<n){
+            //agar ans  me se s[i] hta de win size increase hoga 
             if(s[j]=='a'){
                 co_a--;
             }
@@ -31,6 +34,7 @@ public:
                 co_c--;
             }
 
+            //shrink
             while(i<=j && (co_a<k || co_b<k || co_c<k)){
                 if(s[i]=='a'){
                     co_a++;
