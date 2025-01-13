b60ba2ca483cee56a97faaafd24931696577de08
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2025-01-13 18:13:09 +0530

    Time: 28 ms (58.47%), Space: 31.9 MB (86.69%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map<char,int> mp;
-        for(char &ch:s){
-            mp[ch]++;
-            if(mp[ch]>2){
-                mp[ch]-=2;
+        //unordered_map<char,int> mp;
+        //for(char &ch:s){
+        //    mp[ch]++;
+        //    if(mp[ch]>2){
+        //        mp[ch]-=2;
+        //    }
+        //}
+        //int co=0;
+        //for(const auto &pair:mp){
+        //    co+=pair.second;
+        //}
+        //return co;
+
+        //using 26 size vector
+        vector<int> vec(26,0);
+        for(int i=0;i<s.length();i++){
+            vec[s[i]-'a']++;
+            if(vec[s[i]-'a']>2){
+                vec[s[i]-'a']-=2;
             }
         }
         int co=0;
-        for(const auto &pair:mp){
-            co+=pair.second;
+        for(int i=0;i<26;i++){
+            if(vec[i]==0){
+                continue;
+            }
+            co+=vec[i];
         }
         return co;
-        
     }
 };
