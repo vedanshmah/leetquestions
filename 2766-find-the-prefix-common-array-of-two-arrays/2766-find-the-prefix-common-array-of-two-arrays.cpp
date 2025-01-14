862400d2a2dec58dd62a4d1af370bdbc9d876461
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2025-01-14 17:45:35 +0530

    Time: 0 ms (100%), Space: 85.9 MB (76.89%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        unordered_map<int,int> mp;
-        int co=0;
-        int n=A.size();
-        vector<int> res(n);
-        for(int i=0;i<n;i++){
-            mp[A[i]]++;
-            if(mp[A[i]]==2){
-                co++;
+        //unordered_map<int,int> mp;
+        //int co=0;
+        //int n=A.size();
+        //vector<int> res(n);
+        //for(int i=0;i<n;i++){
+        //    mp[A[i]]++;
+        //    if(mp[A[i]]==2){
+        //        co++;
+        //    }
+        //    mp[B[i]]++;
+        //    if(mp[B[i]]==2){
+        //        co++;
+        //    }
+        //    res[i]=co;
+        //}
+        //return res;
+        int n = A.size();
+        vector<bool> seen(51, false);
+        vector<int> ans;
+        int common = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (seen[A[i]]) {
+                ++common;
+            } else {
+                seen[A[i]] = true;
             }
-            mp[B[i]]++;
-            if(mp[B[i]]==2){
-                co++;
+
+            if (seen[B[i]]) {
+                ++common;
+            } else {
+                seen[B[i]] = true;
             }
-            res[i]=co;
+
+            ans.push_back(common);
         }
-        return res;
+
+        return ans;
     }
 };
