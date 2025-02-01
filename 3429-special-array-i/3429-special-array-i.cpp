1e8fa0b33de94bd806957d0d8bdc8dc10d989b31
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2025-02-01 05:44:09 +0530

    Time: 0 ms (100%), Space: 27.5 MB (99.24%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -6,12 +6,12 @@ public:
             return true;
         }
         for(int i=1;i<n;i++){
-            if(nums[i]%2==0 && nums[i-1]%2==0){
+            if(nums[i]%2==0 && nums[i-1]%2==0 || nums[i]%2!=0 && nums[i-1]%2!=0){
                 return false;
             }
-            else if(nums[i]%2!=0 && nums[i-1]%2!=0){
-                return false;
-            }
+            //else if(nums[i]%2!=0 && nums[i-1]%2!=0){
+            //    return false;
+            //}
         }
         return true;
     }
