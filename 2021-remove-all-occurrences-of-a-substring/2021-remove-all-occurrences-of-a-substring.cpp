de76062611a1c4b1ce3ed20d29086dee6a4b9e96
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2025-02-11 21:24:40 +0530

    Time: 0 ms (100%), Space: 12.2 MB (8.29%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -11,26 +11,35 @@ public:
         return true;
     }
     string removeOccurrences(string s, string part) {
-        //while(s.length() != 0 && s.find(part) < s.length()){
-        //    s.erase(s.find(part) , part.length());
-        //}
-        //return s;
-        //using stack
-        stack<char> st;
-        for(int i=0;i<s.size();i++){
-            st.push(s[i]);
-            if(st.size()>=part.size() && check(st,part)){
-                for(int i=1;i<=part.size();i++){
-                    st.pop();
-                }
+        //using string as satck
+        string res="";
+        int n=s.size();
+        int m=part.size();
+        for(auto &ch:s){
+            res.push_back(ch);
+            if(res.size()>=m && res.substr(res.size()-m)==part){
+                res.erase(res.size()-m);
             }
         }
-        string res="";
-        while(!st.empty()){
-            res.push_back(st.top());
-            st.pop();
-        }
-        reverse(res.begin(),res.end());
         return res;
+
+
+        //using stack
+        //stack<char> st;
+        //for(int i=0;i<s.size();i++){
+        //    st.push(s[i]);
+        //    if(st.size()>=part.size() && check(st,part)){
+        //        for(int i=1;i<=part.size();i++){
+        //            st.pop();
+        //        }
+        //    }
+        //}
+        //string res="";
+        //while(!st.empty()){
+        //    res.push_back(st.top());
+        //    st.pop();
+        //}
+        //reverse(res.begin(),res.end());
+        //return res;
     }
 };
