3b980f5f1ab882bd8967c05cbb2c5dc5f3294cb5
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2025-02-11 21:20:34 +0530

    Time: 11 ms (5.85%), Space: 25.4 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,9 +1,36 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        while(s.length() != 0 && s.find(part) < s.length()){
-            s.erase(s.find(part) , part.length());
+    bool check(stack<char> &st,string &part){
+        stack<char> temp=st;
+        for(int i=part.size()-1;i>=0;i--){
+            if(part[i]!=temp.top()){
+                return false;
+            }
+            temp.pop();
         }
-        return s;
+        return true;
+    }
+    string removeOccurrences(string s, string part) {
+        //while(s.length() != 0 && s.find(part) < s.length()){
+        //    s.erase(s.find(part) , part.length());
+        //}
+        //return s;
+        //using stack
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            st.push(s[i]);
+            if(st.size()>=part.size() && check(st,part)){
+                for(int i=1;i<=part.size();i++){
+                    st.pop();
+                }
+            }
+        }
+        string res="";
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
