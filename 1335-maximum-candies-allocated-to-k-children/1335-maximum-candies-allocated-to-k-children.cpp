1a58ab77bc15be6666bcf80d689d8695b7a0ea5e
vedanshmah <140486656+vedanshmah@users.noreply.github.com>
2025-03-14 22:05:02 +0530

    Time: 22 ms (77.72%), Space: 88.3 MB (41.19%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -4,9 +4,9 @@ public:
         long long int res=0;
         for(int i=0;i<candies.size();i++){
             res+=candies[i]/mid;
-        }
-        if(res>=k){
-            return true;
+            if(res>=k){
+                return true;
+            }
         }
         return false;
     }
