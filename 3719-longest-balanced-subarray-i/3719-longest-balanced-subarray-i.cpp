26187daa51e3d44e5728df9fd3454c28e3933fbc
Vedansh Mahendroo <140486656+vedanshmah@users.noreply.github.com>
2026-02-11 00:54:49 +0530

    Time: 500 ms (96.02%), Space: 210.1 MB (97.48%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -4,13 +4,16 @@ public:
         int n=nums.size();
         int ans=0;
         for(int i=0;i<n;i++){
+            if(ans>=n-i){
+                    return ans;
+                }
             unordered_set<int> oddset;
             unordered_set<int> evenset;
             for(int j=i;j<n;j++){
                 if(nums[j]%2==0){
                     evenset.insert(nums[j]);
                 }
-                if(nums[j]%2!=0){
+                else{
                     oddset.insert(nums[j]);
                 }
                 if(oddset.size()==evenset.size()){
