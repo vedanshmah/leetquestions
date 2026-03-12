189dc4ecfe3eb232020dec2c4747ebfd083e979b
Vedansh Mahendroo <140486656+vedanshmah@users.noreply.github.com>
2026-03-12 19:53:16 +0530

    Time: 12 ms (47.63%), Space: 70.8 MB (21.57%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -17,3 +17,8 @@ public:
         return false;
     }
 };
+// we start from n-1 -> 0  because nums2 and nums 3 should be greater ones to satify .. nums[k]<nums[j]
+// the moment we find nums[i]<nums[k]... we find our required subsequence..
+// we store max element in stack bottom .. then smaller ontop 
+// the moment we find an elemnt greater than top of stack element we consider it as num2 and top ele as num3 thats why we update nums3=top and pop it till stack is empty 
+// the moment we find nums[i] < nums3 we are done as nums3 is already <nums2 
